233d7032218f0e0ff9761316f5331b163e9fb6a2
callandor19 <123434862+callandor19@users.noreply.github.com>
2025-06-20 15:50:19 -0500

    Add files via upload

---
+++ factionData.js
@@ -0,0 +1,756 @@
+const factionData = {
+  "Anglo-Saxons": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 2,
+          "Javelin": 10,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 35,
+          "Banner": 50,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 2,
+          "Javelin": 10,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 35,
+          "Banner": 50,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Huscarl": {
+      "base": 40,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {
+          "Horse": 35
+        }
+      }
+    },
+    "Healer": {
+      "base": 40,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    }
+  },
+  "Bretons": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 15,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 0,
+          "Hand Weapon": 15,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 5,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 20,
+          "Banner": 50,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 15,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 0,
+          "Hand Weapon": 15,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 5,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 20,
+          "Banner": 50,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Healer": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    },
+    "Wagon": {
+      "base": 30,
+      "options": {
+        "Protection": {},
+        "Melee Weapon": {},
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    }
+  },
+  "Byzantines": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Pike": 10
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 25,
+          "Banner": 45,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Pike": 10
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 25,
+          "Banner": 45,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Kataphraktos": {
+      "base": 20,
+      "options": {
+        "Protection": {
+          "Armour and Shield": 10
+        },
+        "Melee Weapon": {
+          "Spear and Hand Weapon": 15
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {
+          "Horse": 25
+        }
+      }
+    }
+  },
+  "Franks": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 10,
+          "Javelin": 5,
+          "Bow": 25,
+          "Crossbow": 35
+        },
+        "Special Equipment": {
+          "Horse": 25,
+          "Banner": 20,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 20,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 10,
+          "Javelin": 5,
+          "Bow": 25,
+          "Crossbow": 35
+        },
+        "Special Equipment": {
+          "Horse": 25,
+          "Banner": 20,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Healer": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    },
+    "Wagon": {
+      "base": 40,
+      "options": {
+        "Protection": {},
+        "Melee Weapon": {},
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    }
+  },
+  "Magyars": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 10,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Sabre": 10
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 0,
+          "Composite Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 20
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 10,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Sabre": 10
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 0,
+          "Composite Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 20
+        }
+      }
+    },
+    "Bok": {
+      "base": 25,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 10,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Sabre": 10
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 0,
+          "Composite Bow": 25
+        },
+        "Special Equipment": {
+          "Horse": 20
+        }
+      }
+    }
+  },
+  "Irish, Scots, Picts": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 30,
+          "Shield": 10
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 0,
+          "Hand Weapon": 20,
+          "Dane Axe": 25
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 0,
+          "Bow": 40,
+          "Crossbow": 25
+        },
+	"Special Equipment": {         
+          "Warhorn": 30,
+          "Warhounds": 30
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 30,
+          "Shield": 10
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 0,
+          "Hand Weapon": 20,
+          "Dane Axe": 25
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 0,
+          "Bow": 40,
+          "Crossbow": 25
+        },
+	"Special Equipment": {
+          "Warhorn": 30
+        }
+      }
+    },
+    "Healer": {
+      "base": 25,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {}
+      }
+    }
+  },
+  "Normans": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 5,
+          "Bow": 25,
+          "Crossbow": 30
+        },
+        "Special Equipment": {
+          "Horse": 20,
+          "Banner": 50,
+          "Warhorn": 40
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 20,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 20
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 5,
+          "Bow": 25,
+          "Crossbow": 30
+        },
+        "Special Equipment": {
+          "Horse": 20,
+          "Banner": 50,
+          "Warhorn": 40
+        }
+      }
+    },
+    "Healer": {
+      "base": 35,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    },
+    "Wagon": {
+      "base": 50,
+      "options": {
+        "Protection": {},
+        "Melee Weapon": {},
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    }
+  },
+  "Rus and Varangians": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 15,
+		  "Second Hand Weapon": 15,
+          "Dane Axe": 30
+        },
+        "Ranged Weapon": {
+          "Sling": 10,
+          "Javelin": 15,
+          "Bow": 30
+        },
+        "Special Equipment": {
+          "Horse": 30,
+          "Banner": 50,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 15,
+		  "Second Hand Weapon": 15,
+          "Dane Axe": 30
+        },
+        "Ranged Weapon": {
+          "Sling": 10,
+          "Javelin": 15,
+          "Bow": 30
+        },
+        "Special Equipment": {
+          "Horse": 30,
+          "Banner": 50,
+          "Warhorn": 25
+        }
+      }
+    },
+    "Berserker": {
+      "base": 80,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 15,
+		  "Second Hand Weapon": 15,
+          "Dane Axe": 30
+        },
+        "Ranged Weapon": {
+          "Sling": 10,
+          "Javelin": 15,
+          "Bow": 30
+        },
+        "Special Equipment": {}
+      }
+    }
+  },
+  "Vikings": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 10,
+          "Bow": 20
+	},
+	"Special Equipment": {
+          "Banner": 50,
+          "Warhorn": 20,
+          "Warhounds": 60
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 10,
+          "Bow": 20
+	},
+	"Special Equipment": {
+          "Banner": 50,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Berserker": {
+      "base": 50,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Armour": 5,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 10,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 5,
+          "Javelin": 10,
+          "Bow": 20
+	},
+	"Special Equipment": {
+          "Banner": 50,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Healer": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {}
+      }
+    }
+  },
+  "Welsh": {
+    "Warrior": {
+      "base": 10,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 20,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 0,
+          "Bow": 20
+        },
+        "Special Equipment": {
+          "Horse": 35,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Chieftain": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0,
+          "Shield": 5
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0,
+          "Spear": 5,
+          "Hand Weapon": 20,
+          "Dane Axe": 15
+        },
+        "Ranged Weapon": {
+          "Sling": 0,
+          "Javelin": 0,
+          "Bow": 20
+        },
+        "Special Equipment": {
+          "Horse": 35,
+          "Warhorn": 20
+        }
+      }
+    },
+    "Healer": {
+      "base": 30,
+      "options": {
+        "Protection": {
+          "Unarmoured": 0
+        },
+        "Melee Weapon": {
+          "Improvised Weapon": 0
+        },
+        "Ranged Weapon": {},
+        "Special Equipment": {}
+      }
+    }
+  }
+};
